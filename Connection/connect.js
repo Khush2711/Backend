f029940a07355d522fcea770b43a16f013b8144d
Khush Desai <142734392+Khush2711@users.noreply.github.com>
2023-10-28 18:49:25 +0530

    Delete Connection directory

--- Connection/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DbConnection = async (URL) => {
-    await mongoose.connect(URL)
-        .then(() => { console.log('Database connnected Successfully') })
-        .catch((err) => { console.log(`Connection Fail : ${err}`) });
-};
-
-module.exports = {DbConnection};
